8745a220405ac3ca9bb1f33229aa848dfa95e38e
Shilpa Patwary <shilpa.patwary@sapient.com>
2019-09-19 14:39:30 +0530

    Updates

---
+++ precache-manifest.0ae53e37a251a1114ee3c65b3ada58e5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f10cdda5a063e15e39ec43c36bfe8bf",
+    "url": "/food-recipes/index.html"
+  },
+  {
+    "revision": "0cd13abaa46df0947fc4",
+    "url": "/food-recipes/static/css/3.974727ef.chunk.css"
+  },
+  {
+    "revision": "6e5ab631645cb0fea623",
+    "url": "/food-recipes/static/css/4.f876fd0f.chunk.css"
+  },
+  {
+    "revision": "d529092dcef40ef034dd",
+    "url": "/food-recipes/static/css/main.2613f63d.chunk.css"
+  },
+  {
+    "revision": "cc7b614a09305122084a",
+    "url": "/food-recipes/static/js/2.34b066f0.chunk.js"
+  },
+  {
+    "revision": "0cd13abaa46df0947fc4",
+    "url": "/food-recipes/static/js/3.0ea9cab8.chunk.js"
+  },
+  {
+    "revision": "6e5ab631645cb0fea623",
+    "url": "/food-recipes/static/js/4.0e83f598.chunk.js"
+  },
+  {
+    "revision": "dec5f084f8aec8bec8bd",
+    "url": "/food-recipes/static/js/5.039d243f.chunk.js"
+  },
+  {
+    "revision": "d529092dcef40ef034dd",
+    "url": "/food-recipes/static/js/main.b5ad1169.chunk.js"
+  },
+  {
+    "revision": "7944137808547aaba78d",
+    "url": "/food-recipes/static/js/runtime~main.b0b1c785.js"
+  },
+  {
+    "revision": "6fd1196fdc0bb5f427f40758c27d535e",
+    "url": "/food-recipes/static/media/logo.6fd1196f.svg"
+  }
+]);
---
+++ static/js/5.039d243f.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonpfood-recipes"]=window["webpackJsonpfood-recipes"]||[]).push([[5],{211:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),r=t(58);n.default=function(){return o.a.createElement(r.a,{content:o.a.createElement("span",{className:"page-error text-mega flex justify-center"},"Page Not Found")})}}}]);
+//# sourceMappingURL=5.039d243f.chunk.js.map
